dfc7b01ddc2e87ce590e00d44e36a256fdff0edc
jensen <563299377@qq.com>
2019-03-06 12:33:32 +0800

    commit and push

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import importlib
 import os
 import sys
 
-#加入依赖的默认查找路径
+#加入依赖的默认查找路径,类似于添加环境变量
 #os.path.dirname()功能为去掉文件名，返回目录
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #sys.path.append(BASE_DIR)帮助程序在本项目文件夹下找依赖
@@ -20,7 +20,8 @@ sys.path.append(os.path.join(BASE_DIR, 'utils'))
 
 import provider
 import tf_util
-#用于参数获取，使程序更加友好
+
+#用于命令参数获取，使程序更加友好，个性化运行
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
 parser.add_argument('--model', default='pointnet_cls', help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
@@ -35,7 +36,7 @@ parser.add_argument('--decay_step', type=int, default=200000, help='Decay step f
 parser.add_argument('--decay_rate', type=float, default=0.7, help='Decay rate for lr decay [default: 0.8]')
 FLAGS = parser.parse_args()
 
-#存储参数
+#存储从命令行获取的参数，如未指定，则使用默认参数
 BATCH_SIZE = FLAGS.batch_size
 NUM_POINT = FLAGS.num_point
 MAX_EPOCH = FLAGS.max_epoch
@@ -46,15 +47,16 @@ OPTIMIZER = FLAGS.optimizer
 DECAY_STEP = FLAGS.decay_step
 DECAY_RATE = FLAGS.decay_rate
 
-#导入网络模板
+#导入网络模板，默认使用pointnet_cls
 MODEL = importlib.import_module(FLAGS.model) # import network module
 MODEL_FILE = os.path.join(BASE_DIR, 'models', FLAGS.model+'.py')
+#建立log文件夹
 LOG_DIR = FLAGS.log_dir
 if not os.path.exists(LOG_DIR): os.mkdir(LOG_DIR)
-#把模板文件拷进log文件夹
+#把模板文件和训练文件拷进log文件夹
 os.system('cp %s %s' % (MODEL_FILE, LOG_DIR)) # bkp of model def
 os.system('cp train.py %s' % (LOG_DIR)) # bkp of train procedure
-#打开log日志文件写入基本信息
+#打开log日志文件写入基本信息，LOG_FOUT为操作log文件句柄
 LOG_FOUT = open(os.path.join(LOG_DIR, 'log_train.txt'), 'w')
 LOG_FOUT.write(str(FLAGS)+'\n')
 
@@ -69,7 +71,7 @@ BN_DECAY_CLIP = 0.99
 #获取主机名
 HOSTNAME = socket.gethostname()
 
-# ModelNet40 official train/test split
+# 将储存训练文件名的文件转化成字符串数组['file1','file2','file3','file4',……]
 TRAIN_FILES = provider.getDataFiles( \
     os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/train_files.txt'))
 TEST_FILES = provider.getDataFiles(\
